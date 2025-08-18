d81cc5532c24b7e3edac820d7f8d5b1cc06763bf
rtaylor02 <97712087+rtaylor02@users.noreply.github.com>
2025-08-18 12:22:38 +0100

    Big commit

---
+++ frontend/react-library-app/src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import { HomePage } from "./layouts/HomePage/HomePage";
+import { Footer } from "./layouts/NavbarAndFooter/Footer";
+import { Navbar } from "./layouts/NavbarAndFooter/Navbar";
+
+export const App = () => {
+  return (
+    <>
+      <Navbar/>
+      <HomePage/>
+      <Footer/>
+    </>
+  );
+};
